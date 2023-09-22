53c4980cefecc6267d0965c9e89128d7e4921a39
NaroNong <narol243282@gmail.com>
2023-09-22 12:11:52 +1200

    Do a testing

--- OnlineShopping/src/onlineshopping/OnlineShopping.java
+++ OnlineShopping/src/onlineshopping/OnlineShopping.java
@@ -14,7 +14,7 @@ public class OnlineShopping {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODONOT code application logic here
     }
     
 }
